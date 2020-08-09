9179db4226fe01ecb9c8caae8ad4f6d697a605e9
Emanuel Lauria <emanuel.lauria@pm.me>
2020-08-09 10:41:46 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,56 @@
+(function() {
+  var tradeMath = (function() {
+    return {
+
+      riskReward: function riskReward(dir, entry, exit, stop) {
+        if (!dir || !entry || !exit || !stop) return 0;
+        return Math.round(((exit - entry) / (entry - stop)) * 100) / 100;
+      },
+
+      qty: function qty(balance, risk, entry, stop) {
+        if (balance && risk && entry && stop)
+        return (
+          Math.round(Math.abs(((risk / 100) * balance) / (1 / stop - 1 / entry))) ||
+          0
+        );
+      },
+
+      activeRisk: function activeRisk(balance, entry, stop, qty) {
+        return (
+          Math.round((10000 * (qty * Math.abs(1 / stop - 1 / entry))) / balance) /
+            100 || 0
+        );
+      },
+
+      avgPrice: function avgPrice(q1, p1, q2, p2) {
+        if (!q1 || !q2 || !p2) return p1;
+          return Math.round(((q1 + q2) / (q1 / p1 + q2 / p2)) * 100) / 100;
+      },
+
+      riskPercent: function riskPercent(balance, qty, entry, stop) {
+        if (qty === 0) return 0;
+        if (balance && qty && entry && stop) {
+          var rp = (qty * Math.abs(1 / stop - 1 / entry)) / balance;
+          return Math.round(rp * 10000) / 100;
+        }
+      },
+
+      breakEvenPrice: function breakEvenPrice(direction, total_qty, entry_price, fees) {
+      let be =
+          direction === "short"
+            ? 0.99925 / (1 / entry_price + fees / total_qty)
+            : 1.00075 / (1 / entry_price - fees / total_qty);
+        return Math.round(be * 100) / 100;
+      },
+
+      fixed8: function fixed8(number) {
+        return Math.floor(number * 100000000) / 100000000;
+      }
+    }
+  })();
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = tradeMath;
+  else
+    window.tradeMath = tradeMath;
+})();
